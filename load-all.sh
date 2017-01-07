a5384c48d6bad736e5ee07606e9cb550b97c2ff9
Dave Carlson <dcarlson@xmlmodeling.com>
2017-01-07 14:46:59 -0700

    Update resources

--- load-all.sh
+++ load-all.sh
@@ -11,13 +11,13 @@ curl   -X POST $1 -H 'Content-type: application/json+fhir' -H 'Accept: applicati
 curl   -X POST $1 -H 'Content-type: application/json+fhir' -H 'Accept: application/json+fhir' --data @Common/Common-Patient.json
 curl   -X POST $1 -H 'Content-type: application/json+fhir' -H 'Accept: application/json+fhir' --data @Common/Common-RelatedPerson.json
 
-curl   -X POST $1 -H 'Content-type: application/json+fhir' -H 'Accept: application/json+fhir' --data @CarePlan-LabObservation.json
-curl   -X POST $1 -H 'Content-type: application/json+fhir' -H 'Accept: application/json+fhir' --data @CarePlan-DiagnosticRequest.json
-curl   -X POST $1 -H 'Content-type: application/json+fhir' -H 'Accept: application/json+fhir' --data @CarePlan-VitalSigns.json
-curl   -X POST $1 -H 'Content-type: application/json+fhir' -H 'Accept: application/json+fhir' --data @CarePlan-Condition.json
-curl   -X POST $1 -H 'Content-type: application/json+fhir' -H 'Accept: application/json+fhir' --data @CarePlan-Goal.json
-curl   -X POST $1 -H 'Content-type: application/json+fhir' -H 'Accept: application/json+fhir' --data @CarePlan-MedicationRequest.json
-curl   -X POST $1 -H 'Content-type: application/json+fhir' -H 'Accept: application/json+fhir' --data @CarePlan-CareTeam.json
+curl   -X POST $1 -H 'Content-type: application/json+fhir' -H 'Accept: application/json+fhir' --data @PCP/CarePlan-LabObservation.json
+curl   -X POST $1 -H 'Content-type: application/json+fhir' -H 'Accept: application/json+fhir' --data @PCP/CarePlan-DiagnosticRequest.json
+curl   -X POST $1 -H 'Content-type: application/json+fhir' -H 'Accept: application/json+fhir' --data @PCP/CarePlan-VitalSigns.json
+curl   -X POST $1 -H 'Content-type: application/json+fhir' -H 'Accept: application/json+fhir' --data @PCP/CarePlan-Condition.json
+curl   -X POST $1 -H 'Content-type: application/json+fhir' -H 'Accept: application/json+fhir' --data @PCP/CarePlan-Goal.json
+curl   -X POST $1 -H 'Content-type: application/json+fhir' -H 'Accept: application/json+fhir' --data @PCP/CarePlan-MedicationRequest.json
+curl   -X POST $1 -H 'Content-type: application/json+fhir' -H 'Accept: application/json+fhir' --data @PCP/CarePlan-CareTeam.json
 
 curl   -X POST $1 -H 'Content-type: application/json+fhir' -H 'Accept: application/json+fhir' --data @Dietitian/Dietitian-CarePlan.json
 curl   -X POST $1 -H 'Content-type: application/json+fhir' -H 'Accept: application/json+fhir' --data @PCP/PCP-CarePlan.json
